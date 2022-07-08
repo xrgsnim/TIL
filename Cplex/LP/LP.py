38ffe352d15632c5a5e479f4993d2f86b68ad49f
xrgsnim <xrgsnim@naver.com>
2022-07-08 15:53:21 +0900

    done

---
+++ Cplex/LP/LP.py
@@ -0,0 +1,39 @@
+import cplex
+import docplex.mp
+import sys
+import json
+from docplex.util.environment import get_environment
+from docplex.mp.model import Model
+
+# class LP:
+#     def __init__(self, model, values, constraints):
+#         model = Model(name = '', log_output=True)
+#         self.values = values
+#         self.constraints = constraints
+# 
+#     def add_value(self, value):
+#         value = model.continuous_var(name='value')
+
+
+
+
+
+model = Model(name='LP_example', log_output=True)
+
+x1 = model.continuous_var(name='x1')
+x2 = model.continuous_var(name='x2')
+
+model.maximize(5*x1 + 4*x2)
+
+model.add_constraint(6*x1 + 4*x2 <= 24)
+model.add_constraint(x1 + 2*x2 <= 6)
+model.add_constraint(-1*x1 + x2 <= 1)
+model.add_constraint(x2 <= 2)
+model.add_constraint(x1 >= 0)
+model.add_constraint(x2 >= 0)
+
+
+
+model.print_information()
+sol_model = model.solve()
+model.print_solution()
